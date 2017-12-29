b6497ecb45ad3e44cfe54afcd189aaad747d3a47
Irvin Lim <irvinlimweiquan@gmail.com>
2017-12-30 00:21:39 +0800

    Add Day01 part two

--- solutions/day01.go
+++ solutions/day01.go
@@ -5,22 +5,32 @@ import (
 	"strconv"
 )
 
-// Day01 Inverse Captcha
+// Day01PartOne Inverse Captcha (Part One)
 // http://adventofcode.com/2017/day/1
-func Day01(input string) string {
+func Day01PartOne(input string) string {
 	var sum int
 
 	for i := 0; i < len(input); i++ {
-		var next byte
+		next := input[(i+1)%len(input)]
 
-		// Wrap around the next byte at the last character
-		if i == len(input)-1 {
-			next = input[0]
-		} else {
-			next = input[i+1]
+		if next == input[i] {
+			currInt, _ := strconv.Atoi(fmt.Sprintf("%c", input[i]))
+			sum += currInt
 		}
+	}
 
-		// Increment sum if the next byte matches
+	return fmt.Sprintf("%d", sum)
+}
+
+// Day01PartTwo Inverse Captcha (Part Two)
+// http://adventofcode.com/2017/day/1
+func Day01PartTwo(input string) string {
+	var sum int
+	r := len(input) / 2
+
+	for i := 0; i < len(input); i++ {
+		next := input[(i+r)%len(input)]
+
 		if next == input[i] {
 			currInt, _ := strconv.Atoi(fmt.Sprintf("%c", input[i]))
 			sum += currInt
--- solutions/day01_test.go
+++ solutions/day01_test.go
@@ -5,14 +5,27 @@ import (
 	"testing"
 )
 
-var testcases = []types.TestCase{
+var partOneTests = []types.TestCase{
 	{Input: "1122", Expected: "3"},
 	{Input: "1111", Expected: "4"},
 	{Input: "1234", Expected: "0"},
 	{Input: "91212129", Expected: "9"},
 }
 
-func TestDay01(t *testing.T) {
-	RunTestCases(t, Day01, testcases)
-	RunInputFileTest(t, Day01, "input01", "1049")
+var partTwoTests = []types.TestCase{
+	{Input: "1212", Expected: "6"},
+	{Input: "1221", Expected: "0"},
+	{Input: "123425", Expected: "4"},
+	{Input: "123123", Expected: "12"},
+	{Input: "12131415", Expected: "4"},
+}
+
+func TestDay01PartOne(t *testing.T) {
+	RunTestCases(t, Day01PartOne, partOneTests)
+	RunInputFileTest(t, Day01PartOne, "input01", "1049")
+}
+
+func TestDay01PartTwo(t *testing.T) {
+	RunTestCases(t, Day01PartTwo, partTwoTests)
+	RunInputFileTest(t, Day01PartTwo, "input01", "1508")
 }
